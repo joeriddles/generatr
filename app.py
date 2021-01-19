b1e16ff357440c07096f0c3893e71715922095dc
Joseph Riddle <joeriddles10@gmail.com>
2021-01-18 20:24:59 -0800

    Update Google domains url

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ GOOGLE_URL_PREFIX = 'https://domains.google.com/registrar/search?searchTerm='
 @app.route('/')
 def main():
     sass_word, sass_url = generate()
-    google_sass_url = GOOGLE_URL_PREFIX + sass_word
+    google_sass_url = GOOGLE_URL_PREFIX + sass_word + '.io'
     render_result = render_template('template.html', sass_word=sass_word, sass_url=sass_url, google_sass_url=google_sass_url)
     return render_result
