14672b52efa61ac82c1917a98ebf9487a363689f
Joseph Riddle <joeriddles10@gmail.com>
2021-01-18 22:56:40 -0800

    Replace favicons

--- generatr/generatr.py
+++ generatr/generatr.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 import os
 import random
-import re
 from typing import Tuple
 
 TOP_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
